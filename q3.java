63fbe6684af9f9eb86643d38793b1649fe02e2ae
sumithaha <no@email.com>
2025-03-19 20:51:22 +0530

    third commit

---
+++ q3.java
@@ -0,0 +1,31 @@
+public class q3 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int start = 3;
+        int end = 7;
+        int target = 5;
+        int indexofnumber = ls(arr, start, end, target);
+        if (indexofnumber >= 0) {
+            System.out.println("element is found at index :" + indexofnumber);
+        } else {
+            System.out.println("element is not found");
+
+        }
+
+    }
+
+    static int ls(int[] arr, int start, int end, int target) {
+        if (arr.length <= 0) {
+            return -1;
+
+        }
+
+        for (int index = start; index < end; index++) {
+            if (target == arr[index]) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
